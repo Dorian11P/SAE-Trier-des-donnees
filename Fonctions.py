b6a0ca48762c541a19e014ead56e5f398ea73dc3
Dorian11P <107360989+Dorian11P@users.noreply.github.com>
2023-01-19 11:50:47 +0100

    Update Fonctions.py
    ajout des tris

--- Fonctions.py
+++ Fonctions.py
@@ -44,7 +44,34 @@ def tri_insertion(tab):
                 j -= 1
         tab[j + 1] = k
 
+#recherche itérative
+def recherche_dichotomique( element, liste_triee ):
+    a = 0
+    b = len(liste_triee)-1
+    m = (a+b)//2
+    while a < b :
+        if liste_triee[m] == element :
+            return m
+        elif liste_triee[m] > element :
+            b = m-1
+        else :
+            a = m+1
+        m = (a+b)//2
+    return a
 
+#recherche récursive
+def recherche_dichotomique_recursive( element, liste_triee, a = 0, b = -1 ):
+    if a == b :
+        return a
+    if b == -1 :
+        b = len(liste_triee)-1
+    m = (a+b)//2
+    if liste_triee[m] == element :
+        return m
+    elif liste_triee[m] > element :
+        return recherche_dichotomique_recursive(element, liste_triee, a, m-1)
+    else :
+        return recherche_dichotomique_recursive(element, liste_triee, m+1, b)
 # Programme principale pour tester le code ci-dessus
 
 
